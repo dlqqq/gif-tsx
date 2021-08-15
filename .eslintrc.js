3ff8c6a091fb80b74bdec791697af4eaa19596db
hydrocodone <44106031+diracs-delta@users.noreply.github.com>
2021-08-15 16:57:35 -0700

    symlink .eslintignore and .prettierignore to .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    sourceType: "module",
-  },
   rules: {
     // not needed with typescript
     "react/prop-types": "off",
---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+## 0.1.2
+
+- Symlinked `.eslintignore` and `.prettierignore` to `.gitignore`
+
+- Nuked console log in `useGifController`
+
+## 0.1.1
+
+- Added `bugs` property to `package.json` manifest
+
+## 0.1.0
+
+- Initial release
